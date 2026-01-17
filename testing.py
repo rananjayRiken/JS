1d76184b65ad5806f3105aeeb6b558cf30984d67
regalriken <rikenpatel2005@gmail.com>
2026-01-17 12:07:35 -0500

    pushing from main account

--- testing.py
+++ testing.py
@@ -2,9 +2,3 @@ for i in range(11):
     print(f"Table of {i}")
     for j in range(11):
         print(f"{i}*{j}={i*j}")
-
-print("pushing from regalRiken's account") # failed
-
-print("Try2") # Failed
-
-print("Try3")
